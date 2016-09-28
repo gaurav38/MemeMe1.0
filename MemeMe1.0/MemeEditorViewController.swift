8c61f24d15cf855626e021f894deddc004f372bd
Gaurav Saraf <grv_srf@yahoo.co.in>
2016-09-28 19:58:45 -0400

    Layout works

--- MemeMe1.0/MemeEditorViewController.swift
+++ MemeMe1.0/MemeEditorViewController.swift
@@ -8,19 +8,49 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let defaultTopTextFieldText = "TOP"
+    let defaultBottomTextFieldText = "BOTTOM"
+    
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName : UIColor.black,
+        NSForegroundColorAttributeName : UIColor.white,
+        NSFontAttributeName : UIFont(name : "HelveticaNeue-CondensedBlack", size : 40)!,
+        NSStrokeWidthAttributeName : CGFloat(1)] as [String : Any]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //subscribeToKeyboardNotifications()
+        updateTextFieldsAppearances()
+        topTextField.delegate = self
+        bottomTextField.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)
     }
+    
+    func updateTextFieldsAppearances()
+    {
+        topTextField.text = "TOP"
+        topTextField.defaultTextAttributes = memeTextAttributes
+        topTextField.textAlignment = NSTextAlignment.center
+        topTextField.backgroundColor = UIColor.clear
+        topTextField.borderStyle = UITextBorderStyle(rawValue: 0)!
+        bottomTextField.text = "BOTTOM"
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.textAlignment = NSTextAlignment.center
+        bottomTextField.backgroundColor = UIColor.clear
+        bottomTextField.borderStyle = UITextBorderStyle(rawValue: 0)!
+    }
+    
+    // MARK: UIImagePickerDelegate
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
@@ -32,6 +62,27 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField == topTextField {
+            if textField.text == defaultTopTextFieldText {
+                textField.text = ""
+            }
+        } else if textField == bottomTextField {
+            if textField.text == defaultBottomTextFieldText {
+                textField.text = ""
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // MARK: Pick image IBActions
 
     @IBAction func pickAnImageFromAlbum(_ sender: AnyObject) {
         let controller = UIImagePickerController()
@@ -45,6 +96,21 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         controller.delegate = self
         controller.sourceType = UIImagePickerControllerSourceType.camera
         present(controller, animated: true, completion: nil)
+    }
+    
+    // Move the view when the keyboard covers the text field
+    func keyboardWillShow(notification: NSNotification) {
+        self.view.frame.origin.y -= getKeyboardHeight(notification: notification)
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: Selector(("keyboardWillShow:")), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
     }
 }
 
---
+++ MemeMe1.0/MemeMeTextFieldDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  MemeMeTextFieldDelegate.swift
+//  MemeMe1.0
+//
+//  Created by Gaurav Saraf on 9/27/16.
+//  Copyright © 2016 Gaurav Saraf. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeMeTextFieldDelegate: NSObject, UITextFieldDelegate {
+    let topDefaultText = "TOP"
+    let bottomDefaultText = "BOTTOM"
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.
+    }
+}
